69ef982ff534c29d260e0f8a4cf016fddff0c1f8
Deva-Mari <devamari@gmx.net>
2022-05-27 08:13:53 +1200

    adding Card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
 import React from "react";
+import data from "./data/data.json";
+import Card from "./components/Card";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+    <div>
+      <main>
+        {data.map((item) => {
+          return (
+            <Card
+              key={item.name}
+              name={item.name}
+              picture={item.picture}
+              function={item.function}
+              blurb={item.blurb}
+              quote={item.quote}
+            />
+          );
+        })}
+      </main>
       <div className="attribution">
         Challenge by{" "}
         <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
@@ -25,6 +28,6 @@ function App() {
       </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Card.tsx
@@ -0,0 +1,33 @@
+import styles from "../css/Card.module.css";
+import React, {useState} from "react";
+
+const Card: React.FC<{
+  name: string;
+  picture: string;
+  function: string;
+  blurb: string;
+  quote: string;
+}> = (props) => {
+  // importing client image conditionally
+  const [clientImage, setClientImage] = useState("");
+  let imgName = props.picture;
+  import(`../assets/${imgName}`).then((image) =>
+    setClientImage(image.default)
+  );
+
+  return (
+    <div className={styles.card}>
+      <div className={styles.header}>
+        <img src={clientImage} alt={props.name} />
+        <div>
+          <h2>{props.name}</h2>
+          <p>{props.function}</p>
+        </div>
+      </div>
+      <p className={styles.blurb}>{props.blurb}</p>
+      <p className={styles.quote}>props.quote</p>
+    </div>
+  );
+};
+
+export default Card;
